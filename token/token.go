69b5785413f54dbc358a00b4a1258e2fd2f2eea0
Rob Douma <rdouma81@gmail.com>
2018-05-15 16:34:38 -0400

    More testing

--- token/token.go
+++ token/token.go
@@ -25,9 +25,23 @@ const (
 	LET = "LET"
 )
 
+
 type TokenType string
 
 type Token struct {
 	Type TokenType
 	Literal string
+}
+
+
+var keywords = map[string]TokenType {
+	"fn" : FUNCTION,
+	"let" : LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
