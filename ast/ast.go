7c76a78eb3998b0905b5ee3a709f9a59ef4f3f18
Rob Douma <rdouma81@gmail.com>
2018-05-22 15:37:18 -0400

    Making modifications to AST

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,9 @@
 package ast
 
+import (
+	"Interpreter2/token"
+)
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -18,10 +22,34 @@ type Program struct {
 	Statements []Statement
 }
 
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
+}
+
+
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNodE() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
